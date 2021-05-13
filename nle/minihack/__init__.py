e4109f245c548ace3d1e8450f2114e5a5160bc83
Robert Kirk <robert.kirk.3.14@gmail.com>
2021-05-13 09:56:33 +0100

    minihack: Add memory task
    We use a squeaky board to detect whether the agent has gone on the
    incorrect path, and a grid-bug to detect the correct path. A weak
    monster behind bars is the cue. Currently this isn't very proc-gen, but
    as a prototype I think it's good enough.

--- nle/minihack/__init__.py
+++ nle/minihack/__init__.py
@@ -8,12 +8,12 @@ from nle.minihack.skills import MiniHackSkill
 from nle.minihack.wiki import NetHackWiki
 
 import nle.minihack.envs.room
-import nle.minihack.envs.keyroom
 import nle.minihack.envs.corridor
 import nle.minihack.envs.keyroom
 import nle.minihack.envs.mazewalk
 import nle.minihack.envs.fightcorridor
 import nle.minihack.envs.minigrid
+import nle.minihack.envs.memory
 import nle.minihack.envs.boxohack
 import nle.minihack.envs.river
 import nle.minihack.envs.skills_simple
