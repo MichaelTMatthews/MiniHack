1b8efdc2a6b7ae71967d40e1d9119001a48965bb
MichaelTMatthews <tryfan99@gmail.com>
2021-07-07 17:59:02 +0100

    fix

--- nle/agent/polybeast/polybeast_learner.py
+++ nle/agent/polybeast/polybeast_learner.py
@@ -592,8 +592,6 @@ def train(flags):
         )
         model.load_state_dict(checkpoint_states["model_state_dict"])
         optimizer.load_state_dict(checkpoint_states["optimizer_state_dict"])
-        scheduler.load_state_dict(checkpoint_states["scheduler_state_dict"])
-        stats = checkpoint_states["stats"]
         logging.info(f"Resuming preempted job, current stats:\n{stats}")
     else:
         logging.info("Initialising model from scratch.")
